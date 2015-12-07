2dcf631dd5d7c70796e133bd09281c5d4afed327
okonomiyaki <bakerli@steventaideMac-mini.local>
2015-12-07 23:16:57 +0800

    APP

---
+++ CopyShop/app/src/main/java/com/example/bakerli/copyshop/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bakerli.copyshop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bakerli on 2015/11/23.
+ */
+public class MainActivity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
